bdcf4acb5d63563d2fdc931cec0a2168de0465c5
Abdullah Numan <anewman15@hotmail.com>
2021-08-30 22:00:05 +0600

    Set redis client URL for production in config/sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,5 +9,6 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: 'redis://localhost:6379/0' } if Rails.env.development?
+  config.redis = { url: ENV['REDISTOGO_URL'] } if Rails.env.production?
 end
