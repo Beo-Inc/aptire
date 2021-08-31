2363cfeaba6d82da4aedb11f1ebddee74e8aabc6
Abdullah Numan <anewman15@hotmail.com>
2021-08-31 13:11:14 +0600

    Add API url for action_mailer.default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "dev_point_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'anewman15-dev-point.netlify.app', protocol: 'http' }
+  config.action_mailer.default_url_options = { host: 'anewman15-dev-point.herokuapp.com', protocol: 'https' }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
